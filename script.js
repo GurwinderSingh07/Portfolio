3de48127dd5d0bed50ef51c75babe5b8d9a0b16b
GurwinderSingh07 <142940816+GurwinderSingh07@users.noreply.github.com>
2024-01-11 17:38:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const hearder = document.querySelector("header");
+window.addEventListener("scroll", function(){
+    hearder.classList.toggle("sticky", window.scrollY > 100);
+});
+
+let menu =document.querySelector('#menu-icon');
+let navlist =document.querySelector('.navlist');
+
+
+menu.onclick = () =>{
+ menu.classList.toggle('bx-x');
+ navlist.classList.toggle('open');
+};
+
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+   };
+   
